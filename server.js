fe3521d49705e7d36bc0782a2d723ff63a22fbd7
manali vadwala <manali.vadwala@yahoo.com>
2022-10-12 18:18:15 +0530

    add one app

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const cors = require("cors");
-
+const bodyParser = require("body-parser");
 const app = express();
+const path=require('path');
+// var corsOptions = {
+//   origin: "http://localhost:8081"
+// };
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+app.use(express.static('public'))
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.all('/*',function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
@@ -31,7 +42,8 @@ db.mongoose
 
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome." });
+  res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'))
+  //res.json({ message: "Welcome." });
 });
 
 require("./app/routes/turorial.routes")(app);
