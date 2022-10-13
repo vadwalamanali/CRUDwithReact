94a23b5fb8d6dfcd4f496e95f7546adb749a160a
manali vadwala <manali.vadwala@yahoo.com>
2022-10-13 14:11:58 +0530

    add procifile

--- server.js
+++ server.js
@@ -1,52 +1,47 @@
 const express = require("express");
-const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const path=require('path');
-// var corsOptions = {
-//   origin: "http://localhost:8081"
-// };
-
-// app.use(cors(corsOptions));
 
 
-app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(express.json());
 
-app.use(express.static('public'))
-app.use('./server', express.Router);
-app.use(express.static(path.join(__dirname, './index.html')));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-app.all('/*',function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('public'))
 
-const db = require("./app/models");
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+// app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+// app.all('/*',function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+// app.use(express.urlencoded({ extended: true }));
+
+// const db = require("./app/models");
+// db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => {
+//     console.log("Connected to the database!");
+//   })
+//   .catch(err => {
+//     console.log("Cannot connect to the database!", err);
+//     process.exit();
+//   });
 
 
 app.get("/", (req, res) => {
   //res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'))
-  res.json({ message: "Welcome." });
+  res.send({ message: "Welcome." });
 });
 
-require("./app/routes/turorial.routes")(app);
+//require("./app/routes/turorial.routes")(app);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
