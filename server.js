d3fbd1b453a08650512cd57a6d6b6ae574bec65b
manali vadwala <manali.vadwala@yahoo.com>
2022-10-13 13:19:38 +0530

    add index file

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//app.use(express.static('public'))
+app.use(express.static('public'))
 
-//app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '../index.html')));
 
 app.all('/*',function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
